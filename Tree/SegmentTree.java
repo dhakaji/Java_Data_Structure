a441fa7d75098800b2ed13b6c907bdeda29f5f22
dhakaji <32473246+dhakaji@users.noreply.github.com>
2018-09-10 10:09:20 +0530

    Segment Tree for Range Sum Query

--- Tree/SegmentTree.java
+++ Tree/SegmentTree.java
@@ -7,7 +7,7 @@ public class SegmentTree{
     tree=new int[size];
     buildSegment(arr,0,n-1,0);
   }
-  int buildSegment(int arr[],int first,int last,int index){
+  private int buildSegment(int arr[],int first,int last,int index){
     if(first==last)
     {
       tree[index]=arr[first];
@@ -17,7 +17,7 @@ public class SegmentTree{
     tree[index] =buildSegment(arr,first,mid,index*2+1)+buildSegment(arr,mid+1,last,index*2+2);
     return tree[index];
   }
-  int query(int first,int last,int l,int r,int index){
+  public int query(int first,int last,int l,int r,int index){
     // if(l<first || r>last  || first>last)
     // {
     //   System.out.println("Invalid Input");
@@ -31,14 +31,14 @@ public class SegmentTree{
     int sum=query(first,last,l,mid,index*2+1)+query(first,last,mid+1,r,index*2+2);
     return sum;
   }
-  void update(int arr[],int i,int new_value){
+  public void update(int arr[],int i,int new_value){
     if(i<0 || i>arr.length-1)
     return ;
     int diff= new_value-arr[i];
     arr[i]=new_value;
     updatest(i,0,arr.length-1,diff,0);
   }
-  void updatest(int i,int l,int r,int diff,int index){
+  private void updatest(int i,int l,int r,int diff,int index){
     if(i<l || i>r)
     return ;
     tree[index] +=diff;
@@ -49,12 +49,12 @@ public class SegmentTree{
     }
   }
 
-  public static void main(String[] args) {
-      int []arr=new int[]{1,3,5,7,9,11};
-      SegmentTree st=new SegmentTree(arr);
-      System.out.println("range 1 to 3"+st.query(1,3,0,arr.length-1,0));
-      st.update(arr,1,10);
-      System.out.println("range 1 to 3"+st.query(1,3,0,arr.length-1,0));
+//   public static void main(String[] args) {
+//       int []arr=new int[]{1,3,5,7,9,11};
+//       SegmentTree st=new SegmentTree(arr);
+//       System.out.println("range 1 to 3"+st.query(1,3,0,arr.length-1,0));
+//       st.update(arr,1,10);
+//       System.out.println("range 1 to 3"+st.query(1,3,0,arr.length-1,0));
 
-  }
+//   }
 }
